cea4bb05d5949f71d3998e6845a18983095d4378
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-08-26 23:06:27 +0530

    small change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = require('./config/db');
 const UserModel = require('./model/user_model');
 const TodoModel = require('./model/todo_model');
 
-const port = 5000;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World !!")
